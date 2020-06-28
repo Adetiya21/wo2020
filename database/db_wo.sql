b58dd1152c286109384d9419d44627eef43229e8
Adetiya21 <adetiyaburhasanp45@gmail.com>
2020-06-29 01:46:47 +0700

    fixed home

--- database/db_wo.sql
+++ database/db_wo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 28 Jun 2020 pada 13.32
+-- Waktu pembuatan: 28 Jun 2020 pada 20.02
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.3
 
@@ -76,7 +76,7 @@ CREATE TABLE `tb_invoice` (
 --
 
 INSERT INTO `tb_invoice` (`id`, `email`, `invoice`, `tgl`, `status`, `payment`, `total`, `gambar`) VALUES
-(1, 'member1@member.com', 'HMP-2706200001', '2020-06-27 04:22:57', 'Proses', 'Transfer Bank', 9600000, 'b2c6c02936c6655c5a972a714be683c3.png'),
+(1, 'member1@member.com', 'HMP-2706200001', '2020-06-27 04:22:57', 'Proses', 'Transfer Bank', 9600000, '84c378281f865e692ecfdd6fc7a92d81.png'),
 (2, 'member1@member.com', 'HMP-2706200002', '2020-06-27 04:24:40', 'Proses', 'Cash on Delivery', 5000000, '');
 
 -- --------------------------------------------------------
@@ -122,7 +122,7 @@ CREATE TABLE `tb_member` (
 --
 
 INSERT INTO `tb_member` (`id`, `nama`, `slug`, `email`, `no_telp`, `alamat`, `password`) VALUES
-(1, 'Akun Member 1', 'member-1', 'member1@member.com', '081234567890', 'Jalan Gusti Hamzah No.14', '$2y$10$0sgld.XXXjW7iPIRbIyD0.Vc8HY.eKFvAXRL8QBzo7KWI/ZY8CFbe');
+(1, 'Akun Member 1', 'member-1', 'member1@member.com', '081234567890', 'Jalan Karet Gang Karet Indah', '$2y$10$0sgld.XXXjW7iPIRbIyD0.Vc8HY.eKFvAXRL8QBzo7KWI/ZY8CFbe');
 
 -- --------------------------------------------------------
 
@@ -188,7 +188,7 @@ CREATE TABLE `tb_produk` (
 
 INSERT INTO `tb_produk` (`id`, `id_kategori`, `id_vendor`, `nama`, `harga`, `kuantitas_penjualan`, `deskripsi`, `gambar`, `tgl`, `slug`) VALUES
 (1, 1, 1, 'Henna Kualitas 1', 450000, 9, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '0c26c61e98da8d77ae63055aba3a545e.jpg', '2020-06-05', 'henna-kualitas-1'),
-(2, 3, 1, 'Dekorasi Standart', 5000000, 2, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.				                        ', 'ed405d3e06401d8311021a2ad3ab4136.jpg', '2020-06-27', 'dekorasi-standart'),
+(2, 3, 1, 'Foto Akad Pandemi', 5000000, 3, '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>\r\n', 'ed405d3e06401d8311021a2ad3ab4136.jpg', '2020-06-28', 'foto-akad-pandemi'),
 (3, 3, 1, 'Dekorasi Mewah', 12000000, -1, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '6dca69ea4b3702916602ec97f85cf9e1.jpg', '2020-06-15', 'dekorasi-mewah'),
 (4, 1, 3, 'Henna Sederhana', 100000, 9, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'fe6b5868482deaece9d68f3556a7c742.jpg', '2020-06-21', 'henna-sederhana'),
 (5, 2, 1, 'Makeup Artis', 2000000, 7, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'dfa72e79ec97799147d49bea8f3bd193.jpg', '2020-06-27', 'makeup-artis'),
@@ -240,7 +240,7 @@ CREATE TABLE `tb_vendor` (
 
 INSERT INTO `tb_vendor` (`id`, `nama`, `slug`, `email`, `no_telp`, `ig`, `alamat`, `password`, `status`, `gambar`) VALUES
 (1, 'Vendor 1', 'vendor-1', 'vendor1@vendor.com', '081234567890', 'ade_bput', 'Jalan Ayani 2', '$2y$10$l.XcWP7cilTt7B1/BdaLjOa0eKMxvOJ.eZZFrMVm6NnfHInXLzeVC', 'Diterima', '4a2813f55d870685f47e34c6f67c0c76.jpg'),
-(2, 'Vendor 2', '', 'vendor2@vendor.com', '080987654321', '', 'Jalan H.R.A.Rahman                                                ', '$2y$10$.q72Whwk8pf0qBdALGLGtuWcGdHeTqrbpCkIzVC0/yPD/oGxsLUkC', 'Menunggu', 'ba043cab9d91f5af0bac9cfa125eea80.jpg'),
+(2, 'Vendor 2', 'vendor-2', 'vendor2@vendor.com', '080987654321', '', 'Jalan H.R.A.Rahman                                                ', '$2y$10$.q72Whwk8pf0qBdALGLGtuWcGdHeTqrbpCkIzVC0/yPD/oGxsLUkC', 'Menunggu', 'ba043cab9d91f5af0bac9cfa125eea80.jpg'),
 (3, 'Vendor 3', 'vendor-3', 'vendor3@vendor.com', '089669432192', 'ade_bput', 'Jalan Sulawesi', '$2y$10$7Snc8SrxvPVBgSnwYIlj2ORNXKheKvqnXei05xLskJnPXjQT4aGom', 'Diterima', 'e494f866f4660b05e4f9392bcd595260.jpg');
 
 -- --------------------------------------------------------
@@ -260,7 +260,8 @@ CREATE TABLE `tb_video_produk` (
 --
 
 INSERT INTO `tb_video_produk` (`id`, `id_produk`, `video`) VALUES
-(1, 1, '3884baa6279c87cf1ab3851ab6bbbb5f.mp4');
+(1, 1, '3884baa6279c87cf1ab3851ab6bbbb5f.mp4'),
+(2, 2, '6e01d5c6bb72d4896e41e8e36481b88c.mp4');
 
 --
 -- Indexes for dumped tables
@@ -397,7 +398,7 @@ ALTER TABLE `tb_vendor`
 -- AUTO_INCREMENT untuk tabel `tb_video_produk`
 --
 ALTER TABLE `tb_video_produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -433,6 +434,12 @@ ALTER TABLE `tb_produk`
 --
 ALTER TABLE `tb_ulasan`
   ADD CONSTRAINT `tb_ulasan_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `tb_produk` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_video_produk`
+--
+ALTER TABLE `tb_video_produk`
+  ADD CONSTRAINT `tb_video_produk_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `tb_produk` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
